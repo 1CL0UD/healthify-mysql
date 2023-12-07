e9896fbdbaa77d4aa7089828a2966feb386c5cb2
Kurnia Ramadhani <kurniaraaaaa79@gmail.com>
2023-12-07 21:06:06 +0700

    foods routes

---
+++ routes/foods.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import { getFoods, getFoodById, addFood } from '../handlers/foods.js';
+
+const router = express.Router();
+router.get('/', async (req, res) => {
+  const foods = await getFoods();
+  res.send(foods);
+});
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  const notes = await getFoodById(id);
+  res.status(201).send(notes);
+});
+
+router.post('/', async (req, res) => {
+  const { user_id, food_name, energy, protein, carbohydrate } = req.body;
+  const notes = await addFood(user_id, food_name, energy, protein, carbohydrate);
+  res.send(notes);
+});
+
+export default router;
