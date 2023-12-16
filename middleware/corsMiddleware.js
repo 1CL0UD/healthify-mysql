f21b6a0b7839ecdc0f328c365d430c87ea6270fc
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2023-12-16 22:20:48 +0700

    change cors ip to wildcard

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,5 +1,5 @@
 const setCors = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your frontend URL
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Replace with your frontend URL
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, DELETE, OPTIONS'
