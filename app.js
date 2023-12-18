4f0fffaaa819d1fb7fad21b9afd3d9c928fb3ae4
ShalahuddinAbdul <89372431+1CL0UD@users.noreply.github.com>
2023-12-18 15:57:21 +0700

    removed duplicate setCors import

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ import { verifyToken } from './middleware/verifyToken.js';
 import jwt from 'jsonwebtoken';
 import setCors from './middleware/corsMiddleware.js';
 
-import setCors from './middleware/corsMiddleware.js'; // Import the CORS middleware
-
 const app = express();
 app.use(express.json());
 
